65c3639ad65ad028d4d7424c9f1b8a9e7435574b
Sergey Palamarchuk <spalamarchuk787@gmail.com>
2024-06-15 16:00:03 +0300

    Implement Smart house prototype

---
+++ smart_house/src/main.rs
@@ -0,0 +1,30 @@
+struct _SmartSocket {}
+
+impl _SmartSocket {
+    fn _description(&self) -> String {
+        todo!("Impl text description");
+    }
+
+    fn _turn_on(&mut self) {
+        todo!("Impl turn on functionality");
+    }
+
+    fn _turn_off(&mut self) {
+        todo!("Impl turn off functionality");
+    }
+
+    fn _get_current_power(&self) -> f64 {
+        todo!("Get current power");
+    }
+}
+
+struct _SmartThermometer {}
+
+impl _SmartThermometer {
+    fn _get_current_temp(&self) -> f64 {
+        todo!("Get current temperature");
+    }
+}
+fn main() {
+    println!("Hello, world!");
+}

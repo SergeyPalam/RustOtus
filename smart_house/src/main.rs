5fcbd0dcc904f3ce15d6837b4c0c5eab6b977eb3
Sergey Palamarchuk <spalamarchuk787@gmail.com>
2024-07-12 12:18:28 +0300

    Implement agregation method to arch Smart House

--- smart_house/src/main.rs
+++ smart_house/src/main.rs
@@ -1,8 +1,29 @@
-struct _SmartSocket {}
+use std::collections::HashMap;
+use std::fmt;
 
-impl _SmartSocket {
-    fn _description(&self) -> String {
-        todo!("Impl text description");
+enum DeviceState {
+    Ok(String),
+    #[allow(dead_code)]
+    Fault(String),
+}
+
+impl fmt::Display for DeviceState {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            DeviceState::Ok(cur_state) => write!(f, "{cur_state}"),
+            DeviceState::Fault(fault_reason) => write!(f, "{fault_reason}"),
+        }
+    }
+}
+trait SmartDevice {
+    fn description(&self) -> String;
+    fn get_state(&self) -> DeviceState;
+}
+struct SmartSocket {}
+
+impl SmartSocket {
+    fn new() -> Self {
+        Self {}
     }
 
     fn _turn_on(&mut self) {
@@ -13,18 +34,143 @@ impl _SmartSocket {
         todo!("Impl turn off functionality");
     }
 
-    fn _get_current_power(&self) -> f64 {
-        todo!("Get current power");
+    fn get_current_power(&self) -> f64 {
+        0.0
     }
 }
 
-struct _SmartThermometer {}
-
-impl _SmartThermometer {
-    fn _get_current_temp(&self) -> f64 {
-        todo!("Get current temperature");
+impl SmartDevice for SmartSocket {
+    fn description(&self) -> String {
+        "Smart socket".to_owned()
+    }
+    fn get_state(&self) -> DeviceState {
+        let report = format!(
+            "{}, current power: {}",
+            self.description(),
+            self.get_current_power()
+        );
+        DeviceState::Ok(report)
     }
 }
+
+struct SmartThermometer {}
+
+impl SmartThermometer {
+    fn new() -> Self {
+        Self {}
+    }
+
+    fn get_current_temp(&self) -> f64 {
+        0.0
+    }
+}
+
+impl SmartDevice for SmartThermometer {
+    fn description(&self) -> String {
+        "Smart thermometer".to_owned()
+    }
+    fn get_state(&self) -> DeviceState {
+        let report = format!(
+            "{}, current temperature: {}",
+            self.description(),
+            self.get_current_temp()
+        );
+        DeviceState::Ok(report)
+    }
+}
+
+struct Room {
+    devices: HashMap<String, Box<dyn SmartDevice>>,
+}
+
+impl Room {
+    fn new() -> Room {
+        Room {
+            devices: HashMap::new(),
+        }
+    }
+
+    fn add_device(&mut self, dev_name: &str, device: Box<dyn SmartDevice>) {
+        self.devices.insert(dev_name.to_owned(), device);
+    }
+
+    fn _get_device(&self, name: &str) -> Option<&dyn SmartDevice> {
+        self.devices.get(name).map(|val| val.as_ref())
+    }
+
+    fn _get_devices_names<'a>(&'a self) -> Box<dyn Iterator<Item = &str> + 'a> {
+        Box::new(self.devices.keys().map(|name| name.as_str()))
+    }
+
+    fn get_report(&self) -> String {
+        let mut res = String::new();
+        for (dev_name, device) in self.devices.iter() {
+            let dev_report = format!("{dev_name}: {}", device.get_state());
+            res.push_str(&dev_report);
+            res.push('\n');
+        }
+        res.pop();
+        res
+    }
+}
+
+struct SmartHouse {
+    name: String,
+    rooms: HashMap<String, Room>,
+}
+
+impl SmartHouse {
+    fn new(name: &str) -> Self {
+        Self {
+            name: name.to_owned(),
+            rooms: HashMap::new(),
+        }
+    }
+
+    fn _get_rooms_names<'a>(&'a self) -> Box<dyn Iterator<Item = &str> + 'a> {
+        Box::new(self.rooms.keys().map(|name| name.as_str()))
+    }
+
+    fn add_room(&mut self, room_name: &str, room: Room) {
+        self.rooms.insert(room_name.to_owned(), room);
+    }
+
+    fn _get_room(&self, room_name: &str) -> Option<&Room> {
+        self.rooms.get(room_name)
+    }
+
+    fn get_report(&self) -> String {
+        let mut res = String::from(&self.name);
+        res.push('\n');
+        for (room_name, room) in self.rooms.iter() {
+            res.push_str(format!("{room_name}:\n").as_str());
+            res.push_str(&room.get_report());
+            res.push('\n');
+        }
+        res.pop();
+        res
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let smart_soc = Box::new(SmartSocket::new());
+    let smart_therm = Box::new(SmartThermometer::new());
+
+    let mut bedroom = Room::new();
+    bedroom.add_device("dev1", smart_soc);
+    bedroom.add_device("dev2", smart_therm);
+
+    let mut smart_house = SmartHouse::new("My house");
+    smart_house.add_room("bedroom", bedroom);
+
+    let smart_soc = Box::new(SmartSocket::new());
+    let smart_therm = Box::new(SmartThermometer::new());
+
+    let mut kitchen = Room::new();
+    kitchen.add_device("dev1", smart_soc);
+    kitchen.add_device("dev2", smart_therm);
+
+    smart_house.add_room("kitchen", kitchen);
+
+    println!("{}", smart_house.get_report());
 }
